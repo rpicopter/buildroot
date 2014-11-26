fcbc0bb5796c5f3e00e8272ee35127f617b5dc80
kiwi <gregd72002@gmail.com>
2014-11-26 20:34:24 +0000

    Wifi configuration

--- files/post_build_cleanup.sh
+++ files/post_build_cleanup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 TARGET=$1
 mkdir -p $TARGET/etc/init.d/manual
-mv $TARGET/etc/init.d/S60nfs $TARGET/etc/init.d/manual/
 mv $TARGET/etc/init.d/S01logging $TARGET/etc/init.d/manual/
-mv $TARGET/etc/init.d/S13portmap $TARGET/etc/init.d/manual/
 mv $TARGET/etc/init.d/sixad $TARGET/etc/init.d/manual/
 
 exit 0
