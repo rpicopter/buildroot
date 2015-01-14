0c8f4211c8b28186118ecfb82071bf8daedbfbaa
Gregory Dymarek <gregd72002@gmail.com>
2015-01-14 19:43:39 +0000

    Refreshing

--- files/post_build_cleanup.sh
+++ files/post_build_cleanup.sh
@@ -4,5 +4,7 @@ mkdir -p $TARGET/etc/init.d/manual
 mv $TARGET/etc/init.d/S01logging $TARGET/etc/init.d/manual/
 mv $TARGET/etc/init.d/sixad $TARGET/etc/init.d/manual/
 
+cp -f $TARGET/usr/share/terminfo/x/xterm-color $TARGET/usr/share/terminfo/x/xterm-256color
+
 exit 0
 
